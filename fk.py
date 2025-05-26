a6981c55b6e0eb9e379973edac63a0cb7b6b88f4
CroNusCoder <parthasarathisrivastava@gmail.com>
2025-05-26 22:56:10 +0530

    Update on 2025-05-26

--- fk.py
+++ fk.py
@@ -2,4 +2,9 @@ import pandas as pd
 
 dataset = pd.read_csv("data/questions.csv")
 
-print(dataset.head())
+print(dataset.isnull().sum())
+dataset.fillna("null", inplace=True)
+dataset.to_csv("data/questions.csv", index=False)
+
+# print(dataset["answer"].isnull().sum())
+# dataset["answer"] = dataset["answer"].fillna("null").astype(str)
